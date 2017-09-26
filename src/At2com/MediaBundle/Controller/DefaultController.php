ab7f07121706d3279285085153f1750e505fd270
Olivier Fillol <olivier.fillol@geograph.fr>
2017-09-26 16:05:48 +0200

    Installation

--- src/At2com/MediaBundle/Controller/DefaultController.php
+++ src/At2com/MediaBundle/Controller/DefaultController.php
@@ -6,8 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    /**
+     * [indexAction Retourne la vue de l'ensemble des médias]
+     * @return [object] [La vue de tous les médias]
+     */
     public function indexAction()
     {
-        return $this->render('At2comMediaBundle:Default:index.html.twig');
+        $medias = $this -> get('doctrine.orm.entity_manager')
+                        -> getRepository('At2comMediaBundle:Media')
+                        -> findAll();
+
+        return $this->render('At2comMediaBundle:Default:index.html.twig', array(
+            "medias" => $medias
+        ));
     }
 }
---
+++ src/At2com/MediaBundle/Entity/Media.php
@@ -0,0 +1,311 @@
+<?php
+
+namespace At2com\MediaBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Media
+ *
+ * @ORM\Table(name="media")
+ * @ORM\Entity(repositoryClass="At2com\MediaBundle\Repository\MediaRepository")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Media
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=30)
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="radical", type="string", length=32, unique=true)
+     */
+    private $radical;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="extension", type="string", length=3)
+     */
+    private $extension;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updatedAt", type="datetime")
+     */
+    private $updatedAt;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="createdBy", type="integer")
+     */
+    private $createdBy;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="updatedBy", type="integer")
+     */
+    private $updatedBy;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Media
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set radical
+     *
+     * @param string $radical
+     * @return Media
+     */
+    public function setRadical($radical)
+    {
+        $this->radical = $radical;
+
+        return $this;
+    }
+
+    /**
+     * Get radical
+     *
+     * @return string
+     */
+    public function getRadical()
+    {
+        return $this->radical;
+    }
+
+    /**
+     * Set extension
+     *
+     * @param string $extension
+     * @return Media
+     */
+    public function setExtension($extension)
+    {
+        $this->extension = $extension;
+
+        return $this;
+    }
+
+    /**
+     * Get extension
+     *
+     * @return string
+     */
+    public function getExtension()
+    {
+        return $this->extension;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Media
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Media
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set createdAt
+     * @ORM\PrePersist()
+     *
+     * @param \DateTime $createdAt
+     * @return Media
+     */
+    public function setCreatedAt()
+    {
+        $this -> createdAt = new \DateTime();
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     *
+     * @param \DateTime $updatedAt
+     * @return Media
+     */
+    public function setUpdatedAt()
+    {
+        $this->updatedAt = new \DateTime();
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set createdBy
+     * @ORM\PrePersist()
+     *
+     * @param integer $createdBy
+     * @return Media
+     */
+    public function setCreatedBy($createdBy = 1782)
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * Get createdBy
+     *
+     * @return integer
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * Set updatedBy
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     *
+     * @param integer $updatedBy
+     * @return Media
+     */
+    public function setUpdatedBy($updatedBy = 1782)
+    {
+        $this->updatedBy = $updatedBy;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedBy
+     *
+     * @return integer
+     */
+    public function getUpdatedBy()
+    {
+        return $this->updatedBy;
+    }
+}
